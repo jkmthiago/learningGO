45172bcd8e850da53165f54d582651b61383284d
jkmthiago <thiago-eleuterio@hotmail.com>
2024-09-06 17:59:09 +0000

    first time using go, packages and module

---
+++ secondLesson/pkg/auxiliar/exemplo_01.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt" 
+
+// Retorna uma mensagem no console indicando ao usuário escrever algo na tela
+func Escrever() {
+	fmt.Println("Escreve ae po")
+	escreverDenovo()
+}
---
+++ secondLesson/pkg/auxiliar/exemplo_02.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt" 
+
+// Retorna uma mensagem no console indicando ao usuário escrever algo na tela
+func escreverDenovo() {
+	fmt.Println("Escreve ae po vai, duvido!")
+}
